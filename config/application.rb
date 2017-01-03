f39badbe0c6c178acd74431a6b295ee760a02ac1
RALIST <danilov@ralist.ru>
2017-01-03 18:18:17 +0400

    responsive

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module Balloons
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'stylesheets', 'public')
   end
 end

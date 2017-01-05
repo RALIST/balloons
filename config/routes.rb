0129b1c870cfddf3400a40be5ee65011c9ee7ede
RALIST <danilov@ralist.ru>
2017-01-05 14:30:42 +0400

    'namespaces'

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+  root 'delivery/main#index'
+
   namespace :delivery do
     root 'main#index'
   end
 
   namespace :shop do
+    root 'main#index'
+  end
+
+  namespace :admin do
     root 'main#index'
   end
  

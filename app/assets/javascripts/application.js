91becae7323a572c148c9482ff0a180c3aa42653
ralist <danilov@ralist.ru>
2017-01-23 23:31:49 +0400

    'clockpicker'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,10 +15,6 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require jquery.maskedinput
-//= require bootstrap-formhelpers-datepicker
-//= require bootstrap-formhelpers-datepicker.ru_RU
-//= require bootstrap-formhelpers-timepicker
-//= require bootstrap-formhelpers-timepicker_ru
-//= require bootstrap-formhelpers-number
+//= require clockpicker
 //= require_tree .
 

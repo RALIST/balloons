f1f2e907a59f448de1db2d2ac64fc5b57a03537a
RALIST <danilov@ralist.ru>
2017-02-01 14:57:31 +0400

    admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,67 +12,67 @@ require 'nokogiri'
 
 user = User.create(email: 'info@bigairballoons.ru', phone: '79124614168', admin: true, password: 'paduvi30' )
 
-xls = Roo::Spreadsheet.open('./public/price.xlsm')
-start_row = 1
-end_row = 200
-puts xls.last_row
-(start_row..xls.last_row).each do |row|
-  unless xls.cell(row, 'B').blank?
-    page = Nokogiri::HTML(open(xls.cell(row, 'C')))
-    img_url = 'http://sharik.ru' + page.xpath('//img[@class = "main_image"]/@src').to_s
-    Item.create(
-      name:           xls.cell(row, 'B'),
-      made_by:        xls.cell(row, 'D'),
-      price:          xls.cell(row, 'G'),
-      barcode:        xls.cell(row, 'I'),
-      item_type:      'latex',
-      collection:     xls.cell(row, 'J'),
-      min_order:      xls.cell(row, 'E'),
-      quantity_type:  xls.cell(row, 'F'),
-      img_remote_url: img_url
-      )
-  end
-end
+# xls = Roo::Spreadsheet.open('./public/price.xlsm')
+# start_row = 1
+# end_row = 200
+# puts xls.last_row
+# (start_row..xls.last_row).each do |row|
+#   unless xls.cell(row, 'B').blank?
+#     page = Nokogiri::HTML(open(xls.cell(row, 'C')))
+#     img_url = 'http://sharik.ru' + page.xpath('//img[@class = "main_image"]/@src').to_s
+#     Item.create(
+#       name:           xls.cell(row, 'B'),
+#       made_by:        xls.cell(row, 'D'),
+#       price:          xls.cell(row, 'G'),
+#       barcode:        xls.cell(row, 'I'),
+#       item_type:      'latex',
+#       collection:     xls.cell(row, 'J'),
+#       min_order:      xls.cell(row, 'E'),
+#       quantity_type:  xls.cell(row, 'F'),
+#       img_remote_url: img_url
+#       )
+#   end
+# end
 
-xls = Roo::Spreadsheet.open('./public/price_foil.xlsm')
-start_row = 1
-end_row = 200
-(start_row..xls.last_row).each do |row|
-  unless xls.cell(row, 'B').blank?
-    page = Nokogiri::HTML(open(xls.cell(row, 'C')))
-    img_url = 'http://sharik.ru' + page.xpath('//img[@class = "main_image"]/@src').to_s
-    Item.create(
-      name:           xls.cell(row, 'B'),
-      made_by:        xls.cell(row, 'D'),
-      price:          xls.cell(row, 'G'),
-      barcode:        xls.cell(row, 'I'),
-      item_type:      'foil',
-      collection:     xls.cell(row, 'J'),
-      min_order:      xls.cell(row, 'E'),
-      quantity_type:  xls.cell(row, 'F'),
-      img_remote_url: img_url
-      )
-  end
-end
+# xls = Roo::Spreadsheet.open('./public/price_foil.xlsm')
+# start_row = 1
+# end_row = 200
+# (start_row..xls.last_row).each do |row|
+#   unless xls.cell(row, 'B').blank?
+#     page = Nokogiri::HTML(open(xls.cell(row, 'C')))
+#     img_url = 'http://sharik.ru' + page.xpath('//img[@class = "main_image"]/@src').to_s
+#     Item.create(
+#       name:           xls.cell(row, 'B'),
+#       made_by:        xls.cell(row, 'D'),
+#       price:          xls.cell(row, 'G'),
+#       barcode:        xls.cell(row, 'I'),
+#       item_type:      'foil',
+#       collection:     xls.cell(row, 'J'),
+#       min_order:      xls.cell(row, 'E'),
+#       quantity_type:  xls.cell(row, 'F'),
+#       img_remote_url: img_url
+#       )
+#   end
+# end
 
 
-xls = Roo::Spreadsheet.open('./public/price_supplies.xlsm')
-start_row = 1
-end_row = 200
-(start_row..xls.last_row).each do |row|
-  unless xls.cell(row, 'B').blank?
-    page = Nokogiri::HTML(open(xls.cell(row, 'C')))
-    img_url = 'http://sharik.ru' + page.xpath('//img[@class = "main_image"]/@src').to_s
-    Item.create(
-      name:           xls.cell(row, 'B'),
-      made_by:        xls.cell(row, 'D'),
-      price:          xls.cell(row, 'G'),
-      barcode:        xls.cell(row, 'I'),
-      item_type:      'supplies',
-      collection:     xls.cell(row, 'J'),
-      min_order:      xls.cell(row, 'E'),
-      quantity_type:  xls.cell(row, 'F'),
-      img_remote_url: img_url
-      )
-  end
-end
+# xls = Roo::Spreadsheet.open('./public/price_supplies.xlsm')
+# start_row = 1
+# end_row = 200
+# (start_row..xls.last_row).each do |row|
+#   unless xls.cell(row, 'B').blank?
+#     page = Nokogiri::HTML(open(xls.cell(row, 'C')))
+#     img_url = 'http://sharik.ru' + page.xpath('//img[@class = "main_image"]/@src').to_s
+#     Item.create(
+#       name:           xls.cell(row, 'B'),
+#       made_by:        xls.cell(row, 'D'),
+#       price:          xls.cell(row, 'G'),
+#       barcode:        xls.cell(row, 'I'),
+#       item_type:      'supplies',
+#       collection:     xls.cell(row, 'J'),
+#       min_order:      xls.cell(row, 'E'),
+#       quantity_type:  xls.cell(row, 'F'),
+#       img_remote_url: img_url
+#       )
+#   end
+# end

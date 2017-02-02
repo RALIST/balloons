24c8ac84c18ffe17a7919187d43bb4cecd3add7f
RALIST <danilov@ralist.ru>
2017-02-02 20:24:46 +0400

    orders stats

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,9 +1,16 @@
 class Call < ApplicationRecord
 
-after_save :send_new_call_notification
+after_save :send_new_call_notification, :send_sms_to_admin
 
   private
   def send_new_call_notification
     AdminMailer.new_call_order_notify(self).deliver_now
+  end
+
+  def send_sms_to_admin
+    new_call = 'Заявка на обратный звонок: ' + self.userphone.to_s + ' ' + self.username + '' + self.messangers.join(',')
+    message = MainsmsApi::Message.new(sender: 'sendertest', message: new_order,
+                                      recipients: ['79124532598'])
+    response = message.deliver
   end
 end

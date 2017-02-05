8a862cbc2ba5e272bf87a816b7a0f9ecba3eb27c
RALIST <danilov@ralist.ru>
2017-02-05 14:33:33 +0400

    ssl

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  mount LetsencryptPlugin::Engine, at: '/'
   match "/404", to: "errors#not_found", :via => :all
   match "/500", to: "errors#internal_server_error", :via => :all
 

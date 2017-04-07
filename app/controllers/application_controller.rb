27438812d1959cbfe5d48b889861df733afcb08b
RALIST <danilov@ralist.ru>
2017-04-07 15:18:55 +0400

    font assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :new_call
+  before_action :new_call, :cors_set_access_control_headers
 
   def new_call
     @call = Call.new
   end
 
-  def set_headers
-    headers["Content-Type"]
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
   end
 end

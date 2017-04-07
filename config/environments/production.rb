3ab3d3fa58819aebdacdfa9ecac166ff9dd7b94f
RALIST <danilov@ralist.ru>
2017-04-07 12:44:33 +0400

    font assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,6 +104,7 @@ Rails.application.configure do
           access_key_id: ENV['AWS_ACCESS_KEY'],
           secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
         }
+        s3_host_alias: 'd3605nnx1xhip8.cloudfront.net'
     }
   end
 end

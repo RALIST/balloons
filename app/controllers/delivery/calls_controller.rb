3fcdd5fa49a1e56bceb18a22459d01046a8bea68
RALIST <danilov@ralist.ru>
2017-04-11 20:19:28 +0400

    send sms

--- app/controllers/delivery/calls_controller.rb
+++ app/controllers/delivery/calls_controller.rb
@@ -3,6 +3,8 @@ class Delivery::CallsController < Delivery::DeliveryController
   def create
     @call = Call.create(call_params)
     if @call.save
+      @call.send_sms_to_admin
+      @call.send_new_call_notification
       redirect_to root_path
       flash[:success] = 'Спасибо за заявку! Мы свяжемся с вами в ближайшее время!'
     end
--- app/models/call.rb
+++ app/models/call.rb
@@ -1,8 +1,7 @@
 class Call < ApplicationRecord
 
-after_save :send_new_call_notification, :send_sms_to_admin
+# after_save :send_new_call_notification, :send_sms_to_admin
 
-  private
   def send_new_call_notification
     AdminMailer.new_call_order_notify(self).deliver_now
   end
@@ -10,7 +9,7 @@ after_save :send_new_call_notification, :send_sms_to_admin
   def send_sms_to_admin
     new_call = 'Заявка на обратный звонок: ' + self.userphone.to_s + ' ' + self.username + ' ' + (self.messangers ? self.messangers.join(',') : "")
     message = MainsmsApi::Message.new(sender: 'sendertest', message: new_call,
-                                      recipients: ['79124532598', '79124614168'])
+                                      recipients: ['79124532598'])
     response = message.deliver
   end
 end

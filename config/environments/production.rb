2cc26db355c99990dd4efbd2a81684423ef7a10e
RALIST <danilov@ralist.ru>
2017-04-13 12:34:51 +0400

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,21 +97,17 @@ Rails.application.configure do
     config.paperclip_defaults = {
       # default_url: 'http://s3-us-west-2.amazonaws.com/flashcards123/images/missing/:style/missing_:style.png',
       storage: :s3,
-        s3_region: ENV['AWS_REGION'],
-        s3_host_name: "s3-us-west-2.amazonaws.com",
-        s3_host_alias: 'd18psmjp7qnrxg.cloudfront.net',
-        url: ":s3_alias_url",
-        path: ':class/:attachment/:id_partition/:style/:filename',
-        s3_headers: { 'Expires': 1.year.from_now.httpdate },
-        s3_credentials: {
-          bucket: ENV['AWS_BUCKET'],
-          access_key_id: ENV['AWS_ACCESS_KEY'],
-          secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-        }
-        compression: {
-          :png => '-0 7',
-          :jpeg => '-optimize'
-        }
+      s3_region: ENV['AWS_REGION'],
+      s3_host_name: "s3-us-west-2.amazonaws.com",
+      s3_host_alias: 'd18psmjp7qnrxg.cloudfront.net',
+      url: ":s3_alias_url",
+      path: ':class/:attachment/:id_partition/:style/:filename',
+      s3_headers: { 'Expires': 1.year.from_now.httpdate },
+      s3_credentials: {
+        bucket: ENV['AWS_BUCKET'],
+        access_key_id: ENV['AWS_ACCESS_KEY'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      }
     }
   end
   config.action_dispatch.default_headers = {

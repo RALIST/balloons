3d4ea5e50ea06450453b02823d64115eaaefae91
RALIST <danilov@ralist.ru>
2017-05-03 13:47:46 +0400

    cache

--- app/controllers/delivery/tags_controller.rb
+++ app/controllers/delivery/tags_controller.rb
@@ -11,10 +11,30 @@ class Delivery::TagsController < Delivery::DeliveryController
     else
       @tag = Tag.find(params[:id])
     end
-    set_meta_tags title: "Оформление воздушными шарами на #{@tag.name} в Ижевске в компании Шариковая Фея",
-                  description: "Закажите оформление воздушными шарами на #{params[:tag_name]} в Ижевске по недорогой цене в компании Шариковая Фея",
-                  reverse: true
-      @compositions =  Composition.availible.with_tag(@tag.name).order(:price).paginate(page: params[:page], per_page: 6)
+
+    case @tag.name
+    when 'девичник'
+      set_meta_tags title: "Заказать воздушные шары на девичник в Ижевске в компании Шариковая Фея",
+                    description: "Оформить воздушными шарами девичник в Ижевске в компании Шариковая Фея",
+                    reverse: true
+    when 'вечеринка'
+      set_meta_tags title: "Заказать оформление воздушными шарами на вечеринку в Ижевске в компании Шариковая Фея",
+                    description: "Оформить вечеринку воздушными шарами в Ижевске в компании Шариковая Фея",
+                    reverse: true
+    when 'сюрприз'
+      set_meta_tags title: "Заказать оформление воздушными шарами сюрприз в Ижевске в компании Шариковая Фея",
+                    description: "Оформление воздушными шарами сюрприз в Ижевске в компании Шариковая Фея",
+                    reverse: true
+    when 'день рождения'
+      set_meta_tags title: "Заказать оформление воздушными шарами на день рождения по выгодным ценам в Ижевске в компании Шариковая Фея",
+                    description: "Оформить воздушными шарами день рождения мальчика или девочки, дешевые цены в каталоге компании Шариковая Фея",
+                    reverse: true
+    else
+      set_meta_tags title: "Оформление воздушными шарами на #{@tag.name} в Ижевске в компании Шариковая Фея",
+                    description: "Закажите оформление воздушными шарами на #{params[:tag_name]} в Ижевске по недорогой цене в компании Шариковая Фея",
+                    reverse: true
+
+    @compositions =  Composition.availible.with_tag(@tag.name).order(:price).paginate(page: params[:page], per_page: 6)
     respond_to do |format|
       format.html {redirect_to root_path, flash: {danger: 'По запросу ' + @tag.name + ' ничего не найдено!'} unless @compositions.any?}
       format.js if params[:page]

787b066129ec60d4cba0c0435b1e40313d1bded7
RALIST <danilov@ralist.ru>
2017-05-17 15:43:04 +0400

    sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -16,7 +16,7 @@ SitemapGenerator::Sitemap.create do
     add composition_path(c)
   end
 
-  Tag.composition_tags.find_each do |tag|
+  Tag.composition_tags.each do |tag|
     add tag_path(tag)
   end
 

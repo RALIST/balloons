3295aa69fef52ee6ea1228569284026b5f761881
RALIST <danilov@ralist.ru>
2017-05-23 17:43:58 +0400

    new

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w['*.jpeg', '*.jpg']
 Rails.application.config.assets.precompile += %w( mailer.scss )
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 

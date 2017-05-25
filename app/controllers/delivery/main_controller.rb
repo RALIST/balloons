6287f80bb7ec2f8d897792e9ab091440cf2356c4
RALIST <danilov@ralist.ru>
2017-05-25 18:54:02 +0400

    new

--- app/controllers/delivery/main_controller.rb
+++ app/controllers/delivery/main_controller.rb
@@ -3,7 +3,7 @@ class Delivery::MainController < Delivery::DeliveryController
   def index
     set_meta_tags title: 'Заказать оформление воздушными шарами недорого с бесплатной доставкой по Ижевску в компании Шариковая Фея',
                   description: "Доставка на дом и оформление воздушными шарами с гелием круглосуточно по недорогой цене в Ижевске в компании Шариковая Фея"
-    @compositions = Composition.availible.limit(6).offset(rand(10)).order(:price)
+    @compositions = Composition.availible.order(:views).reverse_order.limit(6).order(:price)
     @composition  = Composition.find(params[:id]) if params[:id]
     respond_to do |format|
       format.html

fe120be4e07e2b1e63bddaba4f91b6b0a4194590
RALIST <danilov@ralist.ru>
2017-09-22 16:31:54 +0400

    orders

--- app/controllers/delivery/orders_controller.rb
+++ app/controllers/delivery/orders_controller.rb
@@ -17,12 +17,10 @@ class Delivery::OrdersController < Delivery::DeliveryController
       auto_login(@order.user, should_remember: true)
       @order.user.cart = @cart if @order.user.cart.blank?
     end
-    @cart.positions.each do |p|
-      @order.positions.push(p)
-    end
     if @order.save && @order.user.present?
       @cart.positions.each do |p|
-        @cart.positions.delete(p)
+        @order.positions.push(p)
+        p.update(cart_id: nil)
       end
       @order.send_admin_notification
       @order.user.calculate_discount

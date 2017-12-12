0f8d96902b95321a3f36f91c4b97a51339e5d3f7
RALIST <danilov@ralist.ru>
2017-12-12 12:41:31 +0400

    microdata

--- app/controllers/delivery/compositions_controller.rb
+++ app/controllers/delivery/compositions_controller.rb
@@ -13,11 +13,24 @@ class Delivery::CompositionsController < Delivery::DeliveryController
     receivers = @comp.receivers.map(&:title)
     @tag = Tag.friendly.find(params[:tag_id]) if params[:tag_id]
     @receiver = Receiver.friendly.find(params[:receiver_id]) if params[:receiver_id]
+
     set_meta_tags title: "Композиция № #{@comp.id} из воздушных шаров | Шариковая фея",
                   reverse: true,
                   keywords: 'воздушные шары, заказать воздушные шары, шарики с доставкой, доставка шариков, воздушные шары с доставкой',
                   description: "Композиция из воздушных шаров на #{tags.join(', ')}, #{receivers.join(', ')}",
                   canonical: (composition_path(@comp) if @tag || @receiver)
+
+    set_meta_tags og: {
+      title: "Композиция № #{@comp.id} из воздушных шаров | Шариковая фея",
+      type: 'article',
+      url: composition_path(@comp),
+      description: "Композиция из воздушных шаров на #{tags.join(', ')}, #{receivers.join(', ') if receivers}",
+      image: @comp.img.url(:thumb),
+      price: {
+        amount: @comp.comp_price.round(0),
+        currency: 'RUB'
+      }
+    }
 
   end
 

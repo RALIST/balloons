83d00a7b90477320a730a10da8fb4304bcf829ae
RALIST <danilov@ralist.ru>
2018-01-20 18:17:11 +0400

    feeds

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -3,7 +3,7 @@ class Feedback < ApplicationRecord
                           convert_options: {preview: "-quality 75 -strip -interlace Plane"},
                           processors: [:thumbnail, :paperclip_optimizer],
                           filename_cleaner: Paperclip::FilenameCleaner.new(/\_-/),
-                          default_url: '/missing/:style/missing_:style.png'
+                          default_url: '/missing/small/missing_small.png'
   validates_attachment_content_type :img,
                         content_type: ["image/jpeg", "image/jpg", "image/png"]
 

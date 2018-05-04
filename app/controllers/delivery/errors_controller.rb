809f3dfb3b2482f3cf9a2b18b28a969f25c8e9d9
RALIST <danilov@ralist.ru>
2018-05-04 16:28:12 +0400

    seo

--- app/controllers/delivery/errors_controller.rb
+++ app/controllers/delivery/errors_controller.rb
@@ -2,10 +2,14 @@ class Delivery::ErrorsController < Delivery::DeliveryController
   layout nil
 
   def not_found
+    @disable_header = true
+    @disable_footer = true
     render(status: 404)
   end
 
   def internal_server_error
+    @disable_header = true
+    @disable_footer = true
     render(status: 500)
   end
 end

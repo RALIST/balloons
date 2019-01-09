a3ec6446cdf1f6f6b6047426565f435e0a88e76f
RALIST <danilov@ralist.ru>
2019-01-09 17:01:29 +0400

    price

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,11 +1,11 @@
 class Price < ApplicationRecord
   has_attached_file :price_sheet
   validates_attachment :price_sheet, presence: true,
-                                     content_type: { content_type: [
-                                       'application/vnd.ms-excel',
-                                       'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-                                     ] },
-                                     message: ' Only EXCEL files are allowed.'
+                       content_type:           {content_type: [
+                                                                  'application/vnd.ms-excel',
+                                                                  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                                                              ]},
+                       message:                ' Only EXCEL files are allowed.'
 
   attr_accessor :vendor, :type
   validates :vendor, :type, presence: true
@@ -17,29 +17,29 @@ class Price < ApplicationRecord
       puts file
       xls = Roo::Spreadsheet.open(file, extension: :xlsm)
 
-      start_row = 1
+      start_row    = 1
       price_vendor = Vendor.find_by(name: vendor)
-      price_type = Type.find_by!(name: type) if type.present?
-      (start_row..xls.last_row).each do |row|
-        @product_name = xls.cell(row, 'B').strip.downcase if xls.cell(row, 'B').present?
-        @barcode = xls.cell(row, 'C') if xls.cell(row, 'C').present?
-        @code = xls.cell(row, 'D') if xls.cell(row, 'D').present?
+      price_type   = Type.find_by!(name: type) if type.present?
+      (start_row .. xls.last_row).each do |row|
+        @product_name  = xls.cell(row, 'B').strip.downcase if xls.cell(row, 'B').present?
+        @barcode       = xls.cell(row, 'C') if xls.cell(row, 'C').present?
+        @code          = xls.cell(row, 'D') if xls.cell(row, 'D').present?
         @product_price = xls.cell(row, 'E') if xls.cell(row, 'E').present?
-        @min_order = xls.cell(row, 'F') if xls.cell(row, 'F').present?
-        @subcategory = xls.cell(row, 'G').strip.downcase if xls.cell(row, 'G').present?
+        @min_order     = xls.cell(row, 'F') if xls.cell(row, 'F').present?
+        @subcategory   = xls.cell(row, 'G').strip.downcase if xls.cell(row, 'G').present?
         next if @barcode.blank?
         Product.where(barcode: xls.cell(row, 'C').to_i).first_or_create do |product|
           case price_type.name
-          when 'латексные шары'
-            get_latex(@product_name, price_vendor, product)
-          when 'фольгированные шары'
-            vendor_name = xls.cell(row, 'A').strip.downcase if xls.cell(row, 'A').present?
-            if vendor_name.present?
-              @vendor = Vendor.find_or_create_by!(name: vendor_name) do |vendor|
-                vendor.name = vendor_name
+            when 'латексные шары'
+              get_latex(@product_name, price_vendor, product)
+            when 'фольгированные шары'
+              vendor_name = xls.cell(row, 'A').strip.downcase if xls.cell(row, 'A').present?
+              if vendor_name.present?
+                @vendor = Vendor.find_or_create_by!(name: vendor_name) do |vendor|
+                  vendor.name = vendor_name
+                end
+                get_foil(@product_name, product, @vendor)
               end
-              get_foil(@product_name, product, @vendor)
-            end
           end
         end
       end
@@ -49,10 +49,19 @@ class Price < ApplicationRecord
   def get_latex(name, vendor, product)
     arr = name.encode('UTF-8').split(/[^a-zA-Zа-яА-Я0-9_]/)
     case vendor.name
-    when 'belbal' || 'gemar'
-      @size = Size.find_by(belbal: arr[1].to_i)
-      if @size.present?
-        arr.delete(arr[1])
+      when 'belbal' || 'gemar'
+        @size = Size.find_by(belbal: arr[1].to_i)
+        if @size.present?
+          arr.delete(arr[1])
+        else
+          arr.each do |word|
+            @size = get_size(word, vendor)
+            if @size.present?
+              arr.delete(word)
+              break
+            end
+          end
+        end
       else
         arr.each do |word|
           @size = get_size(word, vendor)
@@ -61,15 +70,6 @@ class Price < ApplicationRecord
             break
           end
         end
-      end
-    else
-      arr.each do |word|
-        @size = get_size(word, vendor)
-        if @size.present?
-          arr.delete(word)
-          break
-        end
-      end
     end
     arr.each do |word|
       @tone = Tone.find_by(code: word, vendor: vendor)
@@ -87,30 +87,30 @@ class Price < ApplicationRecord
     end
     arr.delete(arr[0])
     if @tone.present? && @texture.present? && @size.present?
-      @item = Latex.find_or_create_by!(vendor: vendor,
-                                       tone: @tone,
+      @item = Latex.find_or_create_by!(vendor:  vendor,
+                                       tone:    @tone,
                                        texture: @texture) do |item|
         item.category = Category.find_or_create_by!(title: 'без рисунка')
-        item.name = arr.join(' ').encode('UTF-8')
+        item.name     = arr.join(' ').encode('UTF-8')
         item.subcategories.push(Subcategory.find_or_create_by!(name: @subcategory))
       end
     else
       @item = Latex.find_or_create_by!(name: arr.join(' ').encode('UTF-8')) do |i|
         i.category = Category.find_or_create_by!(title: 'с рисунком')
-        i.vendor = vendor
-        i.texture = @texture if @texture.present?
-        i.name = arr.join(' ').encode('UTF-8')
-        i.color = @color
+        i.vendor   = vendor
+        i.texture  = @texture if @texture.present?
+        i.name     = arr.join(' ').encode('UTF-8')
+        i.color    = @color
         i.subcategories.push(Subcategory.find_or_create_by!(name: @subcategory))
       end
     end
     if @item.present?
-      product.item = @item
-      product.size = @size
-      product.code = @code
-      product.barcode = @barcode
-      product.price = @product_price
-      product.name = @product_name
+      product.item      = @item
+      product.size      = @size
+      product.code      = @code
+      product.barcode   = @barcode
+      product.price     = @product_price
+      product.name      = @product_name
       product.min_order = @min_order
       product.set_image unless @item.tone
     end
@@ -156,23 +156,23 @@ class Price < ApplicationRecord
     arr.delete(arr[0])
     if @tone.present? && @texture.present? && @form.present?
       @item = Foil.find_or_create_by!(name: arr.join(' ').encode('UTF-8')) do |item|
-        item.vendor = vendor
+        item.vendor    = vendor
         item.foil_form = @form
-        item.texture = @texture
-        item.name = arr.join(' ')
-        item.tone = @tone
-        item.category = Category.find_or_create_by(title: 'без рисунка')
+        item.texture   = @texture
+        item.name      = arr.join(' ')
+        item.tone      = @tone
+        item.category  = Category.find_or_create_by(title: 'без рисунка')
         item.subcategories.push(Subcategory.find_or_create_by!(name: @subcategory))
       end
     else
       @item = Foil.find_or_create_by!(name: arr.join(' ').encode('UTF-8')) do |item|
-        item.vendor = vendor
+        item.vendor    = vendor
         item.foil_form = @form if @form
-        item.texture = @texture if @texture
-        item.name = arr.join(' ').encode('UTF-8')
+        item.texture   = @texture if @texture
+        item.name      = arr.join(' ').encode('UTF-8')
         item.subcategories.push(Subcategory.find_or_create_by!(name: @subcategory))
         if @tone
-          item.tone = @tone
+          item.tone     = @tone
           item.category = Category.find_or_create_by(title: 'без рисунка')
         else
           item.category = Category.find_or_create_by(title: 'с рисунком')
@@ -185,10 +185,10 @@ class Price < ApplicationRecord
       if @form.present? && @form.name == 'цифра' && @size.blank?
         product.size = Size.find_by(in_inch: 40)
       end
-      product.name = name
-      product.barcode = @barcode
-      product.code = @code
-      product.price = @product_price
+      product.name      = name
+      product.barcode   = @barcode
+      product.code      = @code
+      product.price     = @product_price
       product.min_order = @min_order
       product.set_image
     end
@@ -200,10 +200,10 @@ class Price < ApplicationRecord
 
   def get_tone(str, vendor)
     case vendor.name
-    when 'belbal'
-      tone = vendor.tones.find_by(code: format('%03d', str))
-    when 'gemar'
-      tone = vendor.tones.find_by(code: format('%02d', str))
+      when 'belbal'
+        tone = vendor.tones.find_by(code: format('%03d', str))
+      when 'gemar'
+        tone = vendor.tones.find_by(code: format('%02d', str))
     end
   end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,17 +95,17 @@ Rails.application.configure do
   config.force_ssl = true
   unless Rails.env.test?
     config.paperclip_defaults = {
-      default_url: 'https://d3folkhy05ctbw.cloudfront.net/images/missing/small/missing_small.png',
-      storage: :s3,
+        default_url: 'https://s3.eu-central-1.amazonaws.com/images/missing/small/missing_small.png',
+        storage: :s3,
         s3_region: ENV['AWS_REGION'],
-        s3_host_alias: 'd3folkhy05ctbw.cloudfront.net',
-        url: ":s3_alias_url",
-        path: ':class/:attachment/:id_partition/:style/:filename',
+        s3_host_name: "s3.eu-central-1.amazonaws.com",
+        s3_protocol: :https,
+        path: ':class/:attachment/:style/:filename',
         s3_headers: { 'Expires': 1.year.from_now.httpdate },
         s3_credentials: {
-          bucket: ENV['AWS_BUCKET'],
-          access_key_id: ENV['AWS_ACCESS_KEY'],
-          secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+            bucket: ENV['AWS_BUCKET'],
+            access_key_id: ENV['AWS_ACCESS_KEY'],
+            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
         }
     }
   end

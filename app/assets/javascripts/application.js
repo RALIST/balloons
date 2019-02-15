c47e00e123538040b4d760eaf15262d9040391a9
RALIST <danilov@ralist.ru>
2019-02-15 15:23:32 +0400

    'prices'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,24 +1,9 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require popper
 //= require jquery_ujs
 //= require bootstrap
 //= require jquery.maskedinput
 //= require clockpicker
-//= require jquery-ui/widgets/slider
-//= require jquery-ui/effect.all
-//= require jquery.ui.touch-punch
 //= require cookies
 //= require jquery.slick
 //= require social-share-button
@@ -55,7 +40,7 @@ var ready = function(){
   search();
   $('[data-toggle="tooltip"]').tooltip();
   $('body').attr('data-loaded', 'T')
-}
+};
 
 
 var search = function() {
@@ -74,7 +59,7 @@ var search = function() {
 
 var respMenu = function(){
   $('#icon-wrapper').click(function(){
-    $('#resp-menu').toggle('slide',300);
+    $('#resp-menu').slideToggle();
     $(this).find('[data-fa-i2svg]').toggleClass('fa-bars fa-times', 300);
     FontAwesome.dom.i2svg();
   });

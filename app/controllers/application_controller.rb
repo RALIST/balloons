a91a36e0531258813dbece5e40630e7c82825a7b
RALIST <danilov@ralist.ru>
2019-02-26 21:28:01 +0400

    'redirect'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :new_call
+  before_action :new_call, :redirect_subdomain
 
   include MainHelper
 
   private
   def new_call
     @call = Call.new
+  end
+
+  def redirect_subdomain
+    if request.host == 'www.bigairballoons.ru' || 'шариковаяфея.рф' || 'www.шариковаяфея.рф'
+      redirect_to 'https://l2rate.com' + request.fullpath, :status => 301
+    end
   end
 
 end

5658375604b4b1093ad3cff9fba853403fca202e
Kirsan <ralist@inbox.ru>
2019-03-29 22:13:40 +0400

    '123'

--- app/controllers/delivery/categories_controller.rb
+++ app/controllers/delivery/categories_controller.rb
@@ -2,7 +2,7 @@ class Delivery::CategoriesController < Delivery::DeliveryController
 
   def index
     set_meta_tags title: 'Воздушные шары любой тематики с доставкой в %{city} | Шариковая фея' % {city: t("citie.#{@city}.where")}, description: 'Воздушные шары с рисунком - это лучший способ порадовать ребенка! Персонажи любимых мультфильмов, цветы, смайлы и многое другое в ассортименте нашего магазина!'
-    @categories = Rails.cache.fetch('select_categories', expires_in: 1.day) {Subcategory.joins(:products).where (products: {id: Product.availible_products.ids } ).distinct(:name).group('subcategories.id').select("subcategories.slug, subcategories.name, subcategories.id, subcategories.updated_at, COUNT(products.id) as total")}
+    @categories = Rails.cache.fetch('select_categories', expires_in: 1.day) {Subcategory.joins(:products).where(products: {id: Product.availible_products.ids}).distinct(:name).group('subcategories.id').select("subcategories.slug, subcategories.name, subcategories.id, subcategories.updated_at, COUNT(products.id) as total")}
   end
 
   def show
